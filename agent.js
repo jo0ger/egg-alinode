bfcdd147f1a0399ae0a7508e4b34cfb24c131dc4
朱志洋 <zhuzhiyang@meituan.com>
2018-10-06 23:31:42 +0800

    update: alinode async

--- agent.js
+++ agent.js
@@ -1,34 +1,9 @@
 'use strict';
 
-const assert = require('assert');
-const AlinodeAgent = require('agentx');
-const homedir = require('node-homedir');
-const fs = require('fs');
-const path = require('path');
+const start = require('./lib/alinode');
 
 module.exports = agent => {
-  const config = agent.config.alinode;
-  if (!config.enable) {
-    agent.coreLogger.info('[egg-alinode] disable');
-    return;
-  }
-  assert(config.appid, 'config.alinode.appid required');
-  assert(config.secret, 'config.alinode.secret required');
-
-  const nodepathFile = path.join(homedir(), '.nodepath');
-  const nodeBin = path.dirname(process.execPath);
-  fs.writeFileSync(nodepathFile, nodeBin);
-
-  config.logger = agent.coreLogger;
-  config.libMode = true;
-  new AlinodeAgent(config).run();
-  agent.coreLogger.info('[egg-alinode] alinode agentx started, node versions: %j, update %s with %j, config: %j',
-    process.versions,
-    nodepathFile,
-    nodeBin,
-    {
-      server: config.server,
-      appid: config.appid,
-    }
-  );
+  agent.messenger.on('alinode-run', asyncConfig => {
+    start(agent, asyncConfig);
+  });
 };
